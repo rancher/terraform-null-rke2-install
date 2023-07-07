0ace17a839a2e5f2fab52e1d0cfab582165ac1ab
matttrach <matttrach@gmail.com>
2023-07-06 19:33:42 -0500

    propagate docs change
    Signed-off-by: matttrach <matttrach@gmail.com>

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "type" {
   type        = string
   description = <<-EOT
     The designation from the types.tf file to download.
-    Types are currently: linux-amd64, linux-s390x, and windows-amd64.
+    Types are currently: linux-amd64, linux-s390x, and windows-ltsc2022-amd64, and windows-1890-amd64.
     Amd64 types apply to all x86_64 architectures.
     If local_file_path is set, the type and release variables are ignored.
   EOT
@@ -21,7 +21,7 @@ variable "local_file_path" {
   description = <<-EOT
     The path to the directory for the files to use instead of downloading.
     The files should be renamed to match the designations in ./modules/download/types.tf.
-    Currently, the designations are: rke2-images.tar.gz and rke2.tar.gz.
+    Currently, the designations are: rke2-images.tar.gz, rke2.tar.gz, sha256sum.txt, and rke2-install.
     This is useful if the server that is running Terraform does not have access to GitHub.
     If local_file_path is set, the type and release variables are ignored.
   EOT
