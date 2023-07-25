d112dc7ff139ffa937905e09e6956f73ba13072d
matttrach <matttrach@gmail.com>
2023-07-25 14:32:59 -0500

    add a test to the byob example for updating a config, remove reboot actions, make tests pass, update outputs
    Signed-off-by: matttrach <matttrach@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "downloaded_assets" {
   value = (local.local_file_path == "" ? module.download[0].assets : null)
 }
 output "config" {
-  value = (local.rke2_config == "" ? null : module.install.config)
+  value = local.rke2_config
 }
 output "expected_files" {
   value = local.expected_files
--- tests/byob_test.go
+++ tests/byob_test.go
@@ -1,8 +1,11 @@
 package test
 
 import (
-	"github.com/gruntwork-io/terratest/modules/terraform"
+	"os"
 	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/require"
 )
 
 func TestByob(t *testing.T) {
@@ -14,4 +17,25 @@ func TestByob(t *testing.T) {
 
 	defer terraform.Destroy(t, terraformOptions)
 	terraform.InitAndApply(t, terraformOptions)
+}
+
+func TestByobConfigChange(t *testing.T) {
+	t.Parallel()
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../examples/byob",
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
+	newConfig := "\"debug\": \"true\"\n\"node-label\":\n- \"foo=bar\"\n- \"something=amazing\"\n\"tls-san\":\n- \"foo.local\"\n\"write-kubeconfig-mode\": \"0644\"\n"
+	n := []byte(newConfig)
+	err := os.WriteFile("../examples/byob/rke2/rke2-config.yaml", n, 0600)
+	require.NoError(t, err)
+	// add a new config and apply changes
+	terraform.Apply(t, terraformOptions)
+	blankConfig := ""
+	b := []byte(blankConfig)
+	err = os.WriteFile("../examples/byob/rke2/rke2-config.yaml", b, 0600)
+	require.NoError(t, err)
 }
