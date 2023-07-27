56a1bc222546ca2e4d186300a923f386b34fcb7b
matttrach <matttrach@gmail.com>
2023-07-27 16:01:48 -0500

    add teardown function, make sure defers are working properly
    Signed-off-by: matttrach <matttrach@gmail.com>

--- tests/byob_test.go
+++ tests/byob_test.go
@@ -10,57 +10,56 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestByob(t *testing.T) {
-	Setup(t)
-	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: "../examples/byob",
-		Upgrade:      true,
-	})
-
-	defer terraform.Destroy(t, terraformOptions)
-	terraform.InitAndApply(t, terraformOptions)
-}
-
 func TestByobConfigChange(t *testing.T) {
 	Setup(t)
+	defer Teardown(t)
+
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: "../examples/byob",
 		Upgrade:      true,
 	})
-
 	defer terraform.Destroy(t, terraformOptions)
+
 	terraform.InitAndApply(t, terraformOptions)
 
+	// change config
 	newConfig := "\"debug\": \"true\"\n\"node-label\":\n- \"foo=bar\"\n- \"something=amazing\"\n\"tls-san\":\n- \"foo.local\"\n\"write-kubeconfig-mode\": \"0644\"\n"
-	err7 := os.WriteFile("../examples/byob/rke2/rke2-config.yaml", []byte(newConfig), 0600)
-	require.NoError(t, err7)
-	// add a new config and apply changes
-	terraform.Apply(t, terraformOptions)
+	err1 := os.WriteFile("../examples/byob/rke2/rke2-config.yaml", []byte(newConfig), 0600)
+	require.NoError(t, err1)
 
-	// tear down
-	err8 := os.RemoveAll("../examples/byob/rke2")
-	require.NoError(t, err8)
+	// apply again to test config change, this should only copy the config over and not re-install rke2
+	terraform.Apply(t, terraformOptions)
 }
 
 func Setup(t *testing.T) {
-	// set up
 	version := "v1.27.3+rke2r1"
 	url := fmt.Sprintf("https://github.com/rancher/rke2/releases/download/%s", version)
+
 	err := os.RemoveAll("../examples/byob/.terraform")
 	require.NoError(t, err)
 
-	err1 := os.WriteFile("../examples/byob/rke2/rke2-config.yaml", []byte(""), 0600)
+	err1 := os.MkdirAll("../examples/byob/rke2", 0755)
 	require.NoError(t, err1)
 
-	// download rke2 binary, images, sha256sum, and install script
-	err2 := getter.GetAny("../examples/byob/rke2/", fmt.Sprintf("%s/rke2.linux-amd64.tar.gz?archive=false", url))
+	err2 := os.WriteFile("../examples/byob/rke2/rke2-config.yaml", []byte(""), 0600)
 	require.NoError(t, err2)
-	err3 := getter.GetAny("../examples/byob/rke2/", fmt.Sprintf("%s/rke2-images.linux-amd64.tar.gz?archive=false", url))
+
+	// download rke2 binary, images, sha256sum, and install script
+	err3 := getter.GetAny("../examples/byob/rke2/", fmt.Sprintf("%s/rke2.linux-amd64.tar.gz?archive=false", url))
 	require.NoError(t, err3)
-	err4 := getter.GetAny("../examples/byob/rke2/", fmt.Sprintf("%s/sha256sum-amd64.txt", url))
+	err4 := getter.GetAny("../examples/byob/rke2/", fmt.Sprintf("%s/rke2-images.linux-amd64.tar.gz?archive=false", url))
 	require.NoError(t, err4)
-	err5 := getter.GetAny("../examples/byob/rke2/", "https://raw.githubusercontent.com/rancher/rke2/master/install.sh")
+	err5 := getter.GetAny("../examples/byob/rke2/", fmt.Sprintf("%s/sha256sum-amd64.txt", url))
 	require.NoError(t, err5)
-	err6 := os.WriteFile("../examples/byob/rke2/rke2-config.yaml", []byte(""), 0600)
+	err6 := getter.GetAny("../examples/byob/rke2/", "https://raw.githubusercontent.com/rancher/rke2/master/install.sh")
 	require.NoError(t, err6)
+	err7 := os.WriteFile("../examples/byob/rke2/rke2-config.yaml", []byte(""), 0600)
+	require.NoError(t, err7)
+}
+
+func Teardown(t *testing.T) {
+	err := os.RemoveAll("../examples/byob/.terraform")
+	require.NoError(t, err)
+	err1 := os.RemoveAll("../examples/byob/rke2")
+	require.NoError(t, err1)
 }
