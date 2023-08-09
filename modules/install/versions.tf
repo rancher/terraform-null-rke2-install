5105b9960c13a434b71dfdcca892bd801d19f09a
matttrach <matttrach@gmail.com>
2023-08-08 23:43:30 -0500

    adding version constraints to all providers
    Signed-off-by: matttrach <matttrach@gmail.com>

--- modules/install/versions.tf
+++ modules/install/versions.tf
@@ -2,10 +2,12 @@ terraform {
   required_version = ">= 1.0.0"
   required_providers {
     local = {
-      source = "hashicorp/local"
+      source  = "hashicorp/local"
+      version = ">= 2.4"
     }
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
+      version = ">= 3.2"
     }
   }
 }
