c76e3a1db47fa59b52b4bc9585dd386af253acea
matttrach <matttrach@gmail.com>
2023-08-14 13:07:42 -0500

    generating an ssh key in terratest, grabbing the latest release using go in test
    Signed-off-by: matttrach <matttrach@gmail.com>

--- tests/basic_test.go
+++ tests/basic_test.go
@@ -1,29 +1,19 @@
 package test
 
 import (
-	"os"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	"github.com/stretchr/testify/require"
 )
 
 func TestBasic(t *testing.T) {
 	t.Parallel()
-	defer basicTeardown(t)
-
-	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: "../examples/basic",
-		Upgrade:      true,
-	})
-
+	directory := "basic"
+	region := "us-west-1"
+	owner := "terraform-ci@suse.com"
+	terraformVars := map[string]interface{}{}
+	terraformOptions, keyPair, sshAgent := setup(t, directory, region, owner, terraformVars)
+	defer teardown(t, directory, keyPair, sshAgent)
 	defer terraform.Destroy(t, terraformOptions)
 	terraform.InitAndApply(t, terraformOptions)
-}
-
-func basicTeardown(t *testing.T) {
-	err := os.RemoveAll("../examples/basic/.terraform")
-	require.NoError(t, err)
-	err1 := os.RemoveAll("../examples/basic/rke2")
-	require.NoError(t, err1)
 }
