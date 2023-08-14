026df2a351c0a53d27e5daa5ab5e792ac9c2d80b
matttrach <matttrach@gmail.com>
2023-08-14 13:07:42 -0500

    generating an ssh key in terratest, grabbing the latest release using go in test
    Signed-off-by: matttrach <matttrach@gmail.com>

---
+++ examples/basic/variables.tf
@@ -0,0 +1,8 @@
+variable "key" {
+  type    = string
+  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ4HmZ/KHZ/8KsvYlz6wqpoWoOaH1edHId2aK6niqKIw terraform-ci@suse.com"
+}
+variable "key_name" {
+  type    = string
+  default = "terraform-ci"
+}
---
+++ tests/util_test.go
@@ -0,0 +1,82 @@
+package test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	a "github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	aws "github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/ssh"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/require"
+)
+
+func teardown(t *testing.T, directory string, keyPair *aws.Ec2Keypair, sshAgent *ssh.SshAgent) {
+	err := os.RemoveAll(fmt.Sprintf("../examples/%s/.terraform", directory))
+	require.NoError(t, err)
+	err2 := os.RemoveAll(fmt.Sprintf("../examples/%s/rke2", directory))
+	require.NoError(t, err2)
+	err3 := os.RemoveAll(fmt.Sprintf("../examples/%s/tmp", directory))
+	require.NoError(t, err3)
+	err4 := os.RemoveAll(fmt.Sprintf("../examples/%s/.terraform.lock.hcl", directory))
+	require.NoError(t, err4)
+	err5 := os.RemoveAll(fmt.Sprintf("../examples/%s/terraform.tfstate", directory))
+	require.NoError(t, err5)
+	err6 := os.RemoveAll(fmt.Sprintf("../examples/%s/terraform.tfstate.backup", directory))
+	require.NoError(t, err6)
+
+	aws.DeleteEC2KeyPair(t, keyPair)
+	sshAgent.Stop()
+}
+
+func setup(t *testing.T, directory string, region string, owner string, terraformVars map[string]interface{}) (*terraform.Options, *aws.Ec2Keypair, *ssh.SshAgent) {
+	uniqueID := random.UniqueId()
+
+	// Create an EC2 KeyPair that we can use for SSH access
+	keyPairName := fmt.Sprintf("terraform-aws-server-test-%s-%s", directory, uniqueID)
+	keyPair := aws.CreateAndImportEC2KeyPair(t, region, keyPairName)
+
+	// tag the key pair so we can find in the access module
+	client, err1 := aws.NewEc2ClientE(t, region)
+	require.NoError(t, err1)
+
+	input := &ec2.DescribeKeyPairsInput{
+		KeyNames: []*string{a.String(keyPairName)},
+	}
+	result, err2 := client.DescribeKeyPairs(input)
+	require.NoError(t, err2)
+
+	aws.AddTagsToResource(t, region, *result.KeyPairs[0].KeyPairId, map[string]string{"Name": keyPairName, "Owner": owner})
+
+	// start an SSH agent, with our key pair added
+	sshAgent := ssh.SshAgentWithKeyPair(t, keyPair.KeyPair)
+
+	terraformVars["key_name"] = keyPairName
+	terraformVars["key"] = keyPair.KeyPair.PublicKey
+
+	retryableTerraformErrors := map[string]string{
+		// The reason is unknown, but eventually these succeed after a few retries.
+		".*unable to verify signature.*":             "Failed due to transient network error.",
+		".*unable to verify checksum.*":              "Failed due to transient network error.",
+		".*no provider exists with the given name.*": "Failed due to transient network error.",
+		".*registry service is unreachable.*":        "Failed due to transient network error.",
+		".*connection reset by peer.*":               "Failed due to transient network error.",
+		".*TLS handshake timeout.*":                  "Failed due to transient network error.",
+	}
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: fmt.Sprintf("../examples/%s", directory),
+		// Variables to pass to our Terraform code using -var options
+		Vars: terraformVars,
+		// Environment variables to set when running Terraform
+		EnvVars: map[string]string{
+			"AWS_DEFAULT_REGION": region,
+		},
+		SshAgent:                 sshAgent, // Overrides local SSH agent with our new agent
+		RetryableTerraformErrors: retryableTerraformErrors,
+	})
+	return terraformOptions, keyPair, sshAgent
+}
