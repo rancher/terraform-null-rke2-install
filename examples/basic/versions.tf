d3cf6f1c43f2f043baaca7496749eff324f09869
matttrach <matttrach@gmail.com>
2023-08-16 11:44:47 -0500

    add random provider to versions
    Signed-off-by: matttrach <matttrach@gmail.com>

--- examples/basic/versions.tf
+++ examples/basic/versions.tf
@@ -9,6 +9,10 @@ terraform {
       source  = "hashicorp/local"
       version = ">= 2.4"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5.1"
+    }
     # NOTE: this is only required for the examples
     # this is used by the aws_access and aws_server modules
     aws = {
