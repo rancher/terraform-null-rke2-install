ca823678874b0a3da77b48c0a6b5c268be0370a6
matttrach <matttrach@gmail.com>
2023-08-28 16:47:27 -0500

    add descriptions to outputs
    Signed-off-by: matttrach <matttrach@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,14 @@
 output "downloaded_assets" {
-  value = (local.local_file_path == "" ? module.download[0].assets : null)
+  value       = (local.local_file_path == "" ? module.download[0].assets : null)
+  description = <<-EOT
+    A list of the GitHub assets that were downloaded.
+    This will be null if a local file path was set.
+  EOT
 }
 output "expected_files" {
-  value = local.expected_files
+  value       = local.expected_files
+  description = <<-EOT
+    A list of the files that are expected to be present after the download phase.
+    This is static, and is handy to use if you are generating a local file copy.
+  EOT
 }
