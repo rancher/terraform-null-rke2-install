2bbe08b38da1cddeb4ab96c54a4fb8aa6c870665
matttrach <matttrach@gmail.com>
2023-09-14 23:50:25 -0500

    upgrade server mod in examples with new interface
    Signed-off-by: matttrach <matttrach@gmail.com>

--- examples/remote/main.tf
+++ examples/remote/main.tf
@@ -24,17 +24,17 @@ module "aws_access" {
 }
 
 module "aws_server" {
-  depends_on                 = [module.aws_access]
-  source                     = "rancher/server/aws"
-  version                    = "v0.0.11"
-  image                      = "sles-15"
-  server_owner               = local.email
-  server_name                = local.name
-  server_type                = "medium"
-  server_user                = local.username
-  server_ssh_key             = local.public_ssh_key
-  server_subnet_name         = "default"
-  server_security_group_name = module.aws_access.security_group_name
+  depends_on          = [module.aws_access]
+  source              = "rancher/server/aws"
+  version             = "v0.0.12"
+  image               = "sles-15"
+  owner               = local.email
+  name                = local.name
+  type                = "medium"
+  user                = local.username
+  ssh_key             = local.public_ssh_key
+  subnet_name         = "default"
+  security_group_name = module.aws_access.security_group_name
 }
 
 # the default location for the files will be `./rke2`
