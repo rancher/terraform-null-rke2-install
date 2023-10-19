aab5a5ed5c5f0467e2ef0fe666e8ed6e134cfe4f
Matt Trachier <matttrach@gmail.com>
2023-10-18 22:18:53 -0500

    fix: add a pinned terraform version to flake and fix remind workflow (#55)
    * fix: add a pinned terraform version to flake and fix remind workflow
    * fix: try to fix errors in remind workflow
    * fix: try to trigger e2e tests
    * fix: change mapping style
    * fix: try removing names
    ---------
    
    Signed-off-by: matttrach <matttrach@gmail.com>

--- flake.nix
+++ flake.nix
@@ -15,25 +15,25 @@
   # select a tag
   #inputs.nixpkgs.url = "github:NixOS/nixpkgs/21.11";
 
-  # select HEAD on a branch
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
-  # follows allows idempotent loading of nixpkgs in dependent flakes
+  # select packages from another flake
   #inputs.nixpkgs.follows = "nixpkgs/0228346f7b58f1a284fdb1b72df6298b06677495";
-
-  # install flake utils
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  outputs = { self, nixpkgs, flake-utils }:
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-terraform.url = "github:/NixOS/nixpkgs/39ed4b64ba5929e8e9221d06b719a758915e619b";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, nixpkgs-terraform, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       # 'legacy' is not bad, it looks for previously imported nixpkgs
       #  this allows idempotent loading of nixpkgs in dependent flakes
       # https://discourse.nixos.org/t/using-nixpkgs-legacypackages-system-vs-import/17462/8
-      let pkgs = nixpkgs.legacyPackages.${system};
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs-terraform = nixpkgs-terraform.legacyPackages.${system};
       in
       {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
+          buildInputs = with pkgs; with pkgs-terraform; [
             bashInteractive
             git
             terraform
