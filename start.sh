3c51c63bec2a58c28c46ede6abf61978a9704f18
matttrach <matttrach@gmail.com>
2023-10-30 11:05:23 -0500

    fix: systemctl can form an endless loop, use poll with timeout instead
    Signed-off-by: matttrach <matttrach@gmail.com>

--- start.sh
+++ start.sh
@@ -8,8 +8,18 @@ if [ "$(systemctl is-active "${SERVICE_NAME}")" = "active" ]; then
 fi
 systemctl daemon-reload
 systemctl enable "${SERVICE_NAME}"
-{
-  systemctl start "${SERVICE_NAME}"
-} || {
-  systemctl status "rke2-${ROLE}.service"
-}
+systemctl start "${SERVICE_NAME}" &
+
+EXIT=0
+max_attempts=20
+attempts=0
+interval=10
+while [ "$(systemctl is-active "${SERVICE_NAME}")" != "active" ]; do
+  echo "${SERVICE_NAME} status is \"$(systemctl is-active "${SERVICE_NAME}")\""
+  attempts=$((attempts + 1))
+  if [ ${attempts} = ${max_attempts} ]; then EXIT=1; break; fi
+  sleep ${interval};
+done
+echo "${SERVICE_NAME} status is \"$(systemctl is-active "${SERVICE_NAME}")\""
+
+exit $EXIT
