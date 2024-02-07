87d52422af388c4f5ccf1019b9bf6d4c9e81cc52
Matt Trachier <matttrach@gmail.com>
2024-02-07 15:24:12 -0600

    fix: update workflows and flake (#102)
    Signed-off-by: matttrach <matttrach@gmail.com>

--- flake.nix
+++ flake.nix
@@ -29,26 +29,60 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
+        # get "updatecli" tar from release, unpack it, and add it to shell
+        updatecli-version = {
+          "selected" = "v0.66.0";
+        };
+        updatecli-prep = {
+          "x86_64-darwin"   = {
+            "url" = "https://github.com/updatecli/updatecli/releases/download/${updatecli-version.selected}/updatecli_Darwin_x86_64.tar.gz";
+            "sha" = "sha256-AkYYaCp/a4KkZ4zcYc3GepllyPE6bHb7x7K25JSyljY=";
+          };
+          "aarch64-darwin"  = {
+            "url" = "https://github.com/updatecli/updatecli/releases/download/${updatecli-version.selected}/updatecli_Darwin_arm64.tar.gz";
+            "sha" = "sha256-5ctfz4DcHRmjImYzL9hgOaC9pyeYKQxkpdIAr3J1a6g=";
+          };
+          "x86_64-linux"    = {
+            "url" = "https://github.com/updatecli/updatecli/releases/download/${updatecli-version.selected}/updatecli_Linux_x86_64.tar.gz";
+            "sha" = "sha256-tmboI0ew+LApo3uLVqebaa8VA/6rgonGJH2onQEbSyk=";
+          };
+        };
+        updatecli = pkgs.runCommand "updatecli-${updatecli-version.selected}" {} ''
+          cp ${pkgs.fetchzip { # when fetching archives use fetchzip instead of fetchurl to automatically unpack
+            url = updatecli-prep."${system}".url;
+            sha256 = updatecli-prep."${system}".sha;
+            stripRoot = false;
+          }}/updatecli $out
+          chmod +x $out
+        '';
+        updatecli-wrapper = pkgs.writeShellScriptBin "updatecli" ''
+          exec ${updatecli} "$@"
+        '';
+
         # get "leftovers" bin from release and add it to shell
         leftovers-version = {
           # remember when updating the version to also update the shas
           # to get the sha, download the file and run 'nix hash file <file>'
           "selected" = "v0.70.0";
         };
-        leftovers-urls = {
-          "x86_64-darwin" = "https://github.com/genevieve/leftovers/releases/download/${leftovers-version.selected}/leftovers-${leftovers-version.selected}-darwin-amd64";
-          "aarch64-darwin" = "https://github.com/genevieve/leftovers/releases/download/${leftovers-version.selected}/leftovers-${leftovers-version.selected}-darwin-arm64";
-          "x86_64-linux" = "https://github.com/genevieve/leftovers/releases/download/${leftovers-version.selected}/leftovers-${leftovers-version.selected}-linux-amd64";
-        };
-        leftovers-shas = {
-          "x86_64-linux" = "sha256-D2OPjLlV5xR3f+dVHu0ld6bQajD5Rv9GLCMCk9hXlu8=";
-          "x86_64-darwin" = "sha256-HV12kHqB14lGDm1rh9nD1n7Jvw0rCnxmjC9gusw7jfo=";
-          "aarch64-darwin" = "sha256-Tw7G538RYZrwIauN7kI68u6aKS4d/0Efh+dirL/kzoM=";
+        leftovers-prep = {
+          "x86_64-darwin"   = {
+            "url" = "https://github.com/genevieve/leftovers/releases/download/${leftovers-version.selected}/leftovers-${leftovers-version.selected}-darwin-amd64";
+            "sha" = "sha256-HV12kHqB14lGDm1rh9nD1n7Jvw0rCnxmjC9gusw7jfo=";
+          };
+          "aarch64-darwin"  = {
+            "url" = "https://github.com/genevieve/leftovers/releases/download/${leftovers-version.selected}/leftovers-${leftovers-version.selected}-darwin-arm64";
+            "sha" = "sha256-Tw7G538RYZrwIauN7kI68u6aKS4d/0Efh+dirL/kzoM=";
+          };
+          "x86_64-linux"    = {
+            "url" = "https://github.com/genevieve/leftovers/releases/download/${leftovers-version.selected}/leftovers-${leftovers-version.selected}-linux-amd64";
+            "sha" = "sha256-D2OPjLlV5xR3f+dVHu0ld6bQajD5Rv9GLCMCk9hXlu8=";
+          };
         };
         leftovers = pkgs.runCommand "leftovers-${leftovers-version.selected}" {} ''
           cp ${pkgs.fetchurl {
-            url = leftovers-urls."${system}";
-            sha256 = leftovers-shas."${system}";
+            url = leftovers-prep."${system}".url;
+            sha256 = leftovers-prep."${system}".sha;
           }} $out
           chmod +x $out
         '';
@@ -59,24 +93,27 @@
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            act
             actionlint
             bashInteractive
             curl
             git
-            gnupg
-            go
+            gitleaks
+            go # need go for terratest
+            gnupg # need gpg for signing commits
+            jq
             less
-            openssh
+            openssh # need openssh for running remote provisioners
             shellcheck
             tflint
             tfswitch
+            vim
           ];
           shellHook = ''
-            rm -rf "/usr/local/bin/switched-terraform"
-            install -d "/usr/local/bin/switched-terraform"
-            tfswitch -b "/usr/local/bin/switched-terraform/terraform" -d "1.5.7" 1.5.7 > /dev/null;
-            export PATH="$PATH:${leftovers-wrapper}/bin:/usr/local/bin/switched-terraform";
+            homebin=$HOME/bin;
+            install -d $homebin;
+            tfswitch -b $homebin/terraform 1.5.7 &>/dev/null;
+            export PATH="${updatecli-wrapper}/bin:${leftovers-wrapper}/bin:$homebin:$PATH";
+            export PS1="nix:# ";
           '';
         };
       }
