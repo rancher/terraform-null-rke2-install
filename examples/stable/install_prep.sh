fcb95c8227c24507a3d53468bb133aab59eab57e
matttrach <matt.trachier@suse.com>
2024-05-28 21:52:25 -0500

    fix: passing stable test
    Signed-off-by: matttrach <matt.trachier@suse.com>

--- examples/stable/install_prep.sh
+++ examples/stable/install_prep.sh
@@ -8,6 +8,13 @@ if [ "$(id -u)" -ne 0 ]; then
   exit 1
 fi
 
+
+rpm --import https://download.opensuse.org/distribution/leap/15.5/repo/oss/repodata/repomd.xml.key || true
+zypper ar -f https://download.opensuse.org/distribution/leap/15.5/repo/oss/ repo-oss || true
+
+rpm --import https://download.opensuse.org/distribution/leap/15.5/repo/non-oss/repodata/repomd.xml.key || true
+zypper ar -f https://download.opensuse.org/distribution/leap/15.5/repo/non-oss/ repo-non-oss || true
+
 rpm --import https://download.opensuse.org/repositories/security:/SELinux_legacy/15.5/repodata/repomd.xml.key || true
 zypper ar -f https://download.opensuse.org/repositories/security:/SELinux_legacy/15.5/security:SELinux_legacy.repo || true
 rpm --import https://rpm.rancher.io/public.key || true
