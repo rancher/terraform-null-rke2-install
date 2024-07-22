7fb771b52333d388fb55b8733c9eec7b7e352ee7
matttrach <matt.trachier@suse.com>
2024-07-22 18:09:29 -0500

    fix: allow version 6 addresses
    Signed-off-by: matttrach <matt.trachier@suse.com>

--- get_kubeconfig.sh
+++ get_kubeconfig.sh
@@ -7,5 +7,12 @@ export REMOTE_PATH="${2}"
 export IP="${3}"
 export SSH_USER="${4}"
 
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "${SSH_USER}"@"${IP}":"${REMOTE_PATH}" "${FILE}"
+if [ "" != "$(grep ':' <<< ${IP})" ]; then
+  # ipv6
+  scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "${SSH_USER}"@\["${IP}"\]:"${REMOTE_PATH}" "${FILE}"
+else
+  # ipv4
+  scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "${SSH_USER}"@"${IP}":"${REMOTE_PATH}" "${FILE}"
+fi
+
 sed -i "s/127.0.0.1/${IP}/g" "${FILE}" || sed -i '' "s/127.0.0.1/${IP}/g" "${FILE}"
