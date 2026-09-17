630bb83b92d3dfe97705e7d806f0e68ec3852e1f
Matt Trachier <matt.trachier@suse.com>
2026-09-17 01:02:49 -0500

    fix: get kubeconfig errors when not run as root
    Signed-off-by: Matt Trachier <matt.trachier@suse.com>

--- get_kubeconfig.sh
+++ get_kubeconfig.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 set -x
-set -e
+set -eu
 
-export FILE="${1}"
-export REMOTE_PATH="${2}"
-export IP="${3}"
-export SSH_USER="${4}"
+file="${1}"
+remote_path="${2}"
+ip="${3}"
+ssh_user="${4}"
 
-if [ "" != "$(echo "${IP}" | grep ':')" ]; then
-  # ipv6
-  scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "${SSH_USER}"@\["${IP}"\]:"${REMOTE_PATH}" "${FILE}"
-else
-  # ipv4
-  scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "${SSH_USER}"@"${IP}":"${REMOTE_PATH}" "${FILE}"
-fi
+case "${ip}" in
+  *:*)
+    # ipv6
+    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "${ssh_user}"@\["${ip}"\]:"${remote_path}" "${file}"
+    ;;
+  *)
+    # ipv4
+    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "${ssh_user}"@"${ip}":"${remote_path}" "${file}"
+    ;;
+esac
 
-sed -i "s/127.0.0.1/${IP}/g" "${FILE}" || sed -i '' "s/127.0.0.1/${IP}/g" "${FILE}"
-sed -i "s/::1/${IP}/g" "${FILE}" || sed -i '' "s/::1/${IP}/g" "${FILE}"
+sed "s/127.0.0.1/${ip}/g" "${file}" > "${file}.tmp" && mv -f "${file}.tmp" "${file}"
+sed "s/::1/${ip}/g" "${file}" > "${file}.tmp" && mv -f "${file}.tmp" "${file}"
+chmod 0600 "${file}"
